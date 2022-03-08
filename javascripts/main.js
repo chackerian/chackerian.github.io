44f1d4e6010831c078dd1f8ce513c5c8a9cf7db2
Chackerian <nchackerian@gmail.com>
2022-03-08 17:03:33 -0500

    Accordion

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+var acc = document.getElementsByClassName("column-content");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
